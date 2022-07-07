8c6ce738504e79699190931759cb5886ef4c3ac6
harutiro <harutiro2027@outlook.jp>
2022-07-07 20:14:57 +0900

    update:ステータスバーにアイコンの追加

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,18 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
+
+	const myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 10000);
+	const isDevelopment = vscode.env.sessionId === 'someValue.sessionId';
+	const icon = isDevelopment ? '$(debug)' : '$(icon-fire)';
+	const name = vscode.workspace.name;
+	if (name) {
+    	myStatusBarItem.text = `${icon} ${name}`;
+    	myStatusBarItem.show();
+	}
+	context.subscriptions.push(myStatusBarItem);
+
+
 	
 	console.log('Congratulations, your extension "test" is now active!');
 
@@ -11,6 +23,10 @@ export function activate(context: vscode.ExtensionContext) {
 
 
 	let helloOriginal = vscode.commands.registerCommand('test.helloOriginal', () => {
+		if (name) {
+			myStatusBarItem.text = `$(sync~spin) ${icon} ${name}`;
+			myStatusBarItem.show();
+		}
 		vscode.window.showInformationMessage('ようこそSyskenへ');
 	});
 	context.subscriptions.push(helloOriginal);
