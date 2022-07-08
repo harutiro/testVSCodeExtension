25f3f5c73d707b03955625cee97a31f69e25e0a1
harutiro <harutiro2027@outlook.jp>
2022-07-08 10:16:41 +0900

    update:  時間によるインターバルを取る方法を習得した

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const icon = isDevelopment ? '$(debug)' : '$(icon-fire)';
 	const name = vscode.workspace.name;
 	if (name) {
-    	myStatusBarItem.text = `${icon} ${name}`;
+    	myStatusBarItem.text = `${icon} Twitter`;
     	myStatusBarItem.show();
 	}
 	const myCommandId = 'test.helloOriginal';
@@ -20,6 +20,16 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "test" is now active!');
 
 	let disposable = vscode.commands.registerCommand('test.helloWorld', () => {
+		var item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10);
+
+		let printDate = function () {
+			var date = new Date();
+			item.text = date.getHours().toString() + ":" + date.getMinutes().toString() + ":" + date.getSeconds().toString();
+			item.show();
+		};
+	
+		setInterval(printDate,100);
+
 		vscode.window.showInformationMessage('Hello World from test!');
 	});
 	context.subscriptions.push(disposable);
@@ -27,9 +37,21 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let helloOriginal = vscode.commands.registerCommand('test.helloOriginal', () => {
 		if (name) {
-			myStatusBarItem.text = `$(sync~spin) ${icon} ${name}`;
+			myStatusBarItem.text = `$(sync~spin) 読み込み中`;
 			myStatusBarItem.show();
 		}
+
+		var fs=require("fs");
+		var path = require('path');
+		const content = '{"phrase":"Professionally Plagiarize High-quality Ideas"}';
+		const filePath = path.join(vscode.workspace.rootPath, 'twitter.json');
+		fs.writeFileSync(filePath, content, 'utf8');
+
+		const openPath = vscode.Uri.file(filePath);
+			vscode.workspace.openTextDocument(openPath).then(doc => {
+    vscode.window.showTextDocument(doc);
+		});
+
 		vscode.window.showInformationMessage('ようこそSyskenへ');
 	});
 	context.subscriptions.push(helloOriginal);
