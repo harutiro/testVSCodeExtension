c23e88e1a4536aef3c2f7a9f025910c25bc22b2c
Emma Paris <eparis@atlassian.com>
2022-07-09 10:08:05 +0900

    updatre：コメント文の追加

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import axios from 'axios';
 
 // ApiからJsonをもらう時に、型を指定する
+//ここは受け取るデータによって変える
 type password ={
 	phrase: string;
 };
@@ -11,12 +12,16 @@ type password ={
 // };
 
 //実際にApiを叩く部分
+//async:非同期通信で別の場所で作業して結果だけメインに送る
+//Promise型:非同期処理が完了した時結果を返したり、エラーを送る
 async function getPass(): Promise<string> {
 	try{
 		//ここで、Apiを叩いて、パースもしてくれている
 		const { data, status } = await axios.get<password>(
+			//本番はこのURLも変える
 				'https://corporatebs-generator.sameerkumar.website/',
 				{
+					//受け取るデータの情報
 					headers: {
 					Accept: 'application/json',
 				},
@@ -24,16 +29,28 @@ async function getPass(): Promise<string> {
 		);
 
 		//パースしたデータを逆にJsonに戻している
+		//JSON.stringify()は、JavaScriptオブジェクトを取得し、JSON 文字列に変換します
+		//1つ目は出力したいデータで、2つ目は文字列または数値を、返された文字列のスペース（インデント）として使用します
+		console.log("fuck you!");
 		console.log(JSON.stringify(data, null, 4));
 
 		//パースしたデータをコンソールに流す
+		console.log("fuck you!");
 		console.log(data.phrase);
 
 		//APiを取得した時の状態を表示してくれている
 		//成功したら200を返してくれる。
 		//ページがなかったときは404とか
+		//通信プロトコル
+
 		console.log('response status is: ', status);
+
+		//JSONに受け取ったデータを書き出す
+		//JSON.stringify()は、JavaScriptオブジェクトを取得し、JSON 文字列に変換します
+		//1つ目は出力したいデータで、2つ目は文字列または数値を、返された文字列のスペース（インデント）として使用します
 		return JSON.stringify(data, null, 4);
+
+		//エラーが起きた時の処理
 	}catch(error){
 		console.log('error');
 		return 'error';
@@ -69,6 +86,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('test.helloWorld', () => {
 		//ここは、1秒に一回時間取得を読んでくれる部分
 		//本番では、2分に一回盛り上がり度を取得するコードに変更する
+		//２分に一回APIを呼び出すように変更
 		var item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10);
 		let printDate = function () {
 			var date = new Date();
@@ -76,22 +94,24 @@ export function activate(context: vscode.ExtensionContext) {
 			item.show();
 		};
 		//ここで間隔を指定している
-		setInterval(printDate,100);
+		setInterval(printDate,120000);
 
 		vscode.window.showInformationMessage('Hello World from test!');
 	});
 	context.subscriptions.push(disposable);
 
 
+	//
 	let getJson = vscode.commands.registerCommand('test.getJson', () => {
 		//上のメラメラアイコンを押した時にJSonを取得するコード
 		getPass();
 		vscode.window.showInformationMessage('Jsonを取得するよ');
 	});
+	//contextで指定されたアクションを起こした時に関数を呼び出す
 	context.subscriptions.push(getJson);
 
 
-
+	//helloOriginal
 	let helloOriginal = vscode.commands.registerCommand('test.helloOriginal', () => {
 		//ワークスペースが開かれていない時に動くとエラーが出るので、IF文を用いる
 		if (name) {
@@ -100,20 +120,27 @@ export function activate(context: vscode.ExtensionContext) {
 			myStatusBarItem.show();
 
 			//getPassからpromiseの型を返してもらう
+			
 			const getPromise = getPass();
 
 			//プロミスは度非同期処理を行うタイミングで、ちゃんと処理が終了したタイミングで動作をしてくれる関数。
+			//getPromise.then(非同期処理)が終わったタイミングでdataを返す「
 			getPromise.then(data => {
 				//ここからファイルを作成して、表示するコード
+				//ファイルのインスタンス化
 				var fs=require("fs");
 				var path = require('path');
-				//ファイルの作るパスとファイル名をしている
+				//ファイルの作るパスを指定して、twitter.jsonを作成する
 				const filePath = path.join(vscode.workspace.rootPath, 'twitter.json');
-				//実際のファイルの中身を作成する
+				//実際のファイルの中身を作成する(どんなデータを出力するか)
 				fs.writeFileSync(filePath, data, 'utf8');
+				//ファイルのパスを指定
 				const openPath = vscode.Uri.file(filePath);
 				//VSCodeで開いてもらう
+
+				//openTextDocument(openPath)が終わった時docをvscode.window.showTextDocumentに引数として渡す
 				vscode.workspace.openTextDocument(openPath).then(doc => {
+					//filepathを開く
 					vscode.window.showTextDocument(doc);
 				});
 
